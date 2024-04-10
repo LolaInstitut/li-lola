0b9c559d1585ae453c1019af2851fcf0c9eaf740
LolaInstitut <lolagit@li.rs>
2024-04-10 14:48:02 +0200

    Welcome

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Vlado
+          Dobrodosli
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          Biblioteka Lola Instituta d.o.o.
+        </p>
+
       </header>
     </div>
   );
