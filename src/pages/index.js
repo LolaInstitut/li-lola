4556d1d7c7a683f85e55874c57c3af2dbe316723
LolaInstitut <lolagit@li.rs>
2024-04-15 11:44:11 +0200

    Firebase - config

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app'
+import React from 'react'
+
+const firebaseConfig = {
+  apiKey: 
+}
+
+
+export default function index() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
